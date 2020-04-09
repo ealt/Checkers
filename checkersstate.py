b88d4f3279cdfdccfafb7f5db40fceac0abb8c3f
Eric Alt <ealt@mit.edu>
2020-04-08 22:49:24 -0700

    feat: Store sets of player positions in game state
    The players' positions will be used frequently during a game.
    Storing sets of positions prevents multiple iterations over the board.

--- checkersstate.py
+++ checkersstate.py
@@ -5,6 +5,15 @@ class CheckersState:
 
     def __init__(self, board=utils.default_board):
         self._board = board
+        self._get_positions()
+
+    def _get_positions(self):
+        self._positions = (set(), set())
+        for r, row in enumerate(self._board):
+            for c, identity in enumerate(row):
+                if identity != 0:
+                    player = 0 if identity > 0 else 1
+                    self._positions[player].add((r, c))
 
     def visualize(self):
         utils.visualize(self._board)
---
+++ checkersstate_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from checkersstate import CheckersState
+
+
+class CheckersStateTest(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        test_board = [[0, 2], [-1, -1], [0, 1], [-1, 1]]
+        self._cs = CheckersState(test_board)
+
+    def test_get_positions(self):
+        expected_positions = (set([(0, 1), (2, 1), (3, 1)]),
+                              set([(1, 0), (1, 1), (3, 0)]))
+        self.assertCountEqual(self._cs._positions, expected_positions)
+
+
+if __name__ == '__main__':
+    unittest.main()
