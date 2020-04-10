060b2e147472506ec251aa9fa8e4b788922795d0
Eric Alt <ericallenalt@gmail.com>
2020-04-09 22:40:26 -0700

    feat: Store a dict of moves from every board pos
    Piece actions depend on the available moves from their board position.
    Redundant move evaluations are avoided by storing precalculated values.
    Moves are stored as a dictionary with a key for every board position.
    
    From a position, moves can be made in up to four direction.
    These moves can be either to an adjacent empty position or a jump.
    Each move direction has a tuple of the adjacent move and jump move.
    Regular pieces can either move upward or downward but not both.
    Therefore, separate lists of up and down moves are stored as a tuple.

--- checkersstate.py
+++ checkersstate.py
@@ -8,7 +8,71 @@ class CheckersState:
 
     def __init__(self, board=utils.default_board):
         self._board = np.array(board, dtype=np.int32)
+        self._get_moves()
         self._get_positions()
+
+    def _get_moves(self):
+        self._moves = {}
+        num_rows, num_cols = self._board.shape
+        for position in product(range(num_rows), range(num_cols)):
+            self._moves[position] = self._get_moves_from_position(position)
+
+
+    def _get_moves_from_position(self, position):
+        r, c = position
+        r_max, c_max = (n-1 for n in self._board.shape)
+        # moves: (upward moves, downward moves)
+        # move: (empty space move, jump move)
+        moves = ([], [])
+
+        up = r > 0
+        down = r < r_max
+        left = c > 0
+        right = c < c_max
+
+        if r % 2 == 0:
+            if up:
+                if r > 1 and left:
+                    moves[0].append(((r-1, c), (r-2, c-1)))
+                else:
+                    moves[0].append(((r-1, c), None))
+                if right:
+                    if r > 1:
+                        moves[0].append(((r-1, c+1), (r-2, c+1)))
+                    else:
+                        moves[0].append(((r-1, c+1), None))
+            if down:
+                if r < r_max-1 and left:
+                    moves[1].append(((r+1, c), (r+2, c-1)))
+                else:
+                    moves[1].append(((r+1, c), None))
+                if right:
+                    if r < r_max-1:
+                        moves[1].append(((r+1, c+1), (r+2, c+1)))
+                    else:
+                        moves[1].append(((r+1, c+1), None))
+        else:
+            if up:
+                if left:
+                    if r > 1:
+                        moves[0].append(((r-1, c-1), (r-2, c-1)))
+                    else:
+                        moves[0].append(((r-1, c-1), None))
+                if r > 1 and right:
+                    moves[0].append(((r-1, c), (r-2, c+1)))
+                else:
+                    moves[0].append(((r-1, c), None))
+            if down:
+                if left:
+                    if r < r_max-1:
+                        moves[1].append(((r+1, c-1), (r+2, c-1)))
+                    else:
+                        moves[1].append(((r+1, c-1), None))
+                if r < r_max-1 and right:
+                    moves[1].append(((r+1, c), (r+2, c+1)))
+                else:
+                    moves[1].append(((r+1, c), None))
+        return moves
 
     def _get_positions(self):
         self._positions = (set(), set())
--- checkersstate_test.py
+++ checkersstate_test.py
@@ -10,6 +10,27 @@ class CheckersStateTest(unittest.TestCase):
         test_board = [[0, 2], [-1, -1], [0, 1], [-1, 1]]
         self._cs = CheckersState(test_board)
 
+    def test_get_moves(self):
+        expected_moves = {
+            (0, 0): ([],
+                     [((1, 0), None), ((1, 1), (2, 1))]),
+            (0, 1): ([],
+                     [((1, 1), (2, 0))]),
+            (1, 0): ([((0, 0), None)],
+                     [((2, 0), (3, 1))]),
+            (1, 1): ([((0, 0), None), ((0, 1), None)],
+                     [((2, 0), (3, 0)), ((2, 1), None)]),
+            (2, 0): ([((1, 0), None), ((1, 1), (0, 1))],
+                     [((3, 0), None), ((3, 1), None)]),
+            (2, 1): ([((1, 1), (0, 0))],
+                     [((3, 1), None)]),
+            (3, 0): ([((2, 0), (1, 1))],
+                     []),
+            (3, 1): ([((2, 0), (1, 0)), ((2, 1), None)],
+                     [])
+        }
+        self.assertDictEqual(self._cs._moves, expected_moves)
+
     def test_get_positions(self):
         expected_positions = (set([(0, 1), (2, 1), (3, 1)]),
                               set([(1, 0), (1, 1), (3, 0)]))
