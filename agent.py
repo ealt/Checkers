b6445a23e8196e932c9505c079ddf33dd47d1374
Eric Alt <ericallenalt@gmail.com>
2020-04-25 11:02:34 -0700

    feat: Create Pre-programmed agent
    It is useful to test a game by specifying a sequences of agent actions.
    The pre-programmed agent allows for such testing.
    It is initialized with a list of actions.
    Each call to get_action() returns the next action in the list.
    A dummy state is defined to simplify testing.

--- agent.py
+++ agent.py
@@ -5,6 +5,16 @@ class Agent:
         raise NotImplementedError
 
 
+class PreprogrammedAgent(Agent):
+    def __init__(self, actions):
+        self._actions = actions[::-1]
+    
+    def get_action(self, state):
+        action = self._actions.pop()
+        assert(action in state.actions())
+        return action
+
+
 class RandomAgent(Agent):
     def get_action(self, state):
         return choice(state.actions())
--- agent_test.py
+++ agent_test.py
@@ -1,14 +1,45 @@
 import unittest
 
-from agent import Agent, RandomAgent
-from checkersstate import CheckersState
+from agent import Agent, PreprogrammedAgent, RandomAgent
+from checkersstate import State, CheckersState
 
+
+class DummyState(State):
+    def __init__(self, available_actions):
+        self._available_actions = available_actions
+
+    def active_player(self):
+        pass
+
+    def actions(self):
+        return self._available_actions
+
+    def result(self, action):
+        pass
+
+    def is_terminal(self):
+        pass
+
+    def outcome(self):
+        pass
 
 class CheckersStateTest(unittest.TestCase):
 
     def test_base_agent(self):
         with self.assertRaises(NotImplementedError):
             Agent().get_action(CheckersState())
+
+    def test_preprogrammed_agent(self):
+        available_actions = [1, 2, 3]
+        state = DummyState(available_actions)
+        preprogrammed_actions = [2, 1, 1, 3, 2, 3]
+        agent = PreprogrammedAgent(preprogrammed_actions)
+        for expected_action in preprogrammed_actions:
+            self.assertEqual(agent.get_action(state), expected_action)
+        
+        invalid_action = 4
+        with self.assertRaises(AssertionError):
+            PreprogrammedAgent([invalid_action]).get_action(state)
 
     def test_random_agent(self):
         state = CheckersState()
