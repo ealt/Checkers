3c1a5c6c9870c6f2c72e2ab8d8dc2514022050d3
Eric Alt <ericallenalt@gmail.com>
2020-04-25 12:34:46 -0700

    feat: Create human checkers agent
    Create an agent for manual play.
    Calling get_action() displays the board along with a list of actions.
    The user inputs the number corresponding to their chosen action.
    That action is returned by the agent.
    
    resolves #24

--- agent.py
+++ agent.py
@@ -18,3 +18,33 @@ class PreprogrammedAgent(Agent):
 class RandomAgent(Agent):
     def get_action(self, state):
         return choice(state.actions())
+
+class HumanCheckersAgent(Agent):
+    def get_action(self, state):
+        self._num_rows = state._board.shape[0]
+        state.visualize()
+        actions = state.actions()
+        print('\n'.join([str(i + 1) + ': ' + self._describe(action)
+                         for i, action in enumerate(actions)]))
+        selection = int(input('Choose an action from the list above: '))
+        assert(0 < selection <= len(actions))
+        return actions[selection - 1]
+    
+    def _describe(self, action):
+        old_piece_pos, new_piece_pos, jumped_piece_pos = action
+        if jumped_piece_pos:
+            return ('Jump from ' + self._get_label(old_piece_pos)
+                    + ' to ' + self._get_label(new_piece_pos)
+                    + ' over '+ self._get_label(jumped_piece_pos))
+        else:
+            return ('Move from ' + self._get_label(old_piece_pos)
+                    + ' to ' + self._get_label(new_piece_pos))
+    
+    def _get_label(self, pos):
+        row, col = pos
+        col *= 2
+        if row % 2 == 0:
+            col += 1
+        row_label = str(self._num_rows - row)
+        col_label = chr(65 + col)
+        return col_label + row_label
