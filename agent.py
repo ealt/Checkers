568980de01de6bda2f0729d81a26de01f4207fec
Eric Alt <ericallenalt@gmail.com>
2020-04-24 23:33:23 -0700

    feat: Create random agent
    Creates an agent that chooses actions at random.
    Serves as a useful benchmark.
    
    resolves #23

--- agent.py
+++ agent.py
@@ -1,3 +1,10 @@
+from random import choice
+
 class Agent:
     def get_action(self, state):
         raise NotImplementedError
+
+
+class RandomAgent(Agent):
+    def get_action(self, state):
+        return choice(state.actions())
--- agent_test.py
+++ agent_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from agent import Agent
+from agent import Agent, RandomAgent
 from checkersstate import CheckersState
 
 
@@ -9,6 +9,11 @@ class CheckersStateTest(unittest.TestCase):
     def test_base_agent(self):
         with self.assertRaises(NotImplementedError):
             Agent().get_action(CheckersState())
+
+    def test_random_agent(self):
+        state = CheckersState()
+        self.assertIn(RandomAgent().get_action(state), state.actions())
+
 
 if __name__ == '__main__':
     unittest.main()
